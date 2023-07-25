ce783e07b5821bd1dd73a6a2c9f66f612053cb56
Yushirizu <113351099+Yushirizu@users.noreply.github.com>
2023-07-25 21:38:02 +0200

    video 4

--- TOWERDENFECE/Assets/Scripts/WaveSpawner.cs
+++ TOWERDENFECE/Assets/Scripts/WaveSpawner.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class WaveSpawner : MonoBehaviour
 {
@@ -11,6 +12,8 @@ public class WaveSpawner : MonoBehaviour
     public float timeBetweenWaves = 5f;
 
     private float countdown = 2f;
+
+    public Text WaveCountdownText;
 
     private int waveIndex = 0;
 
@@ -23,19 +26,19 @@ public class WaveSpawner : MonoBehaviour
         }
 
         countdown -= Time.deltaTime;
+
+        WaveCountdownText.text = Mathf.Round(countdown).ToString();
     }
 
     IEnumerator SpawnWave()
     {
         waveIndex++;
-        
+
         for (int i = 0; i < waveIndex; i++)
         {
             SpawnEnemy();
             yield return new WaitForSeconds(0.5f);
         }
-
-        
     }
 
     void SpawnEnemy()
